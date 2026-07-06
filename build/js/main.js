c5091534c0a02a390ece2099047b58b39bb625bc
mazax1ct <vova@giveandget.ru>
2026-07-06 19:28:03 +0300

    main-menu + accordion + typo + inner page

--- build/js/main.js
+++ build/js/main.js
@@ -32,98 +32,36 @@ $(document).ready(function() {
 $(window).on("scroll", resize_scroll).on("resize", resize_scroll);
 
 //открытие главного меню
-$(document).on('click', '.js-menu-opener', function () {
+$(document).on('click', '.js-menu-toggler', function () {
   const _this = $(this);
   if(!_this.hasClass('is-active')) {
-    $('body').addClass('is-overflow');
-    $('.header__dropdown.main-menu').addClass('is-open');
-    _this.find('use').attr('xlink:href', 'images/sprite.svg#close_icon');
-    _this.find('span').text('ЗАКРЫТЬ');
+    $('body').addClass('main-menu-open');
+    $('.main-menu').addClass('is-open');
+    _this.find('use').attr('xlink:href', 'images/sprite.svg#close-icon');
     _this.addClass('is-active');
   } else {
-    _this.find('use').attr('xlink:href', 'images/sprite.svg#burger_icon');
-    _this.find('span').text('МЕНЮ');
+    _this.find('use').attr('xlink:href', 'images/sprite.svg#menu-icon');
     _this.removeClass('is-active');
-    $('body').removeClass('is-overflow');
-    $('.header__dropdown.main-menu').removeClass('is-open');
+    $('body').removeClass('main-menu-open');
+    $('.main-menu').removeClass('is-open');
   }
 
-  return false;
-});
-
-//закрытие главного меню
-$(document).on('click', '.js-menu-closer', function () {
-  $('body').removeClass('is-overflow');
-  $('.header__dropdown.main-menu').removeClass('is-open');
-  $('.js-menu-opener').find('use').attr('xlink:href', 'images/sprite.svg#burger_icon');
-  $('.js-menu-opener').find('span').text('МЕНЮ');
-  $('.js-menu-opener').removeClass('is-active');
   return false;
 });
 
 //второй уровень меню
-$(document).on('click', '.js-menu-lvl2', function () {
-  if($(this).hasClass('is-active')) {
-    $(this).removeClass('is-active');
-    $('.main-menu__dropdown').removeClass('is-open');
-    $('.main-menu__inner').removeClass('is-overflow');
-  } else {
-    $('.js-menu-lvl2').removeClass('is-active');
-    $('.main-menu__dropdown').removeClass('is-open');
-    $(this).addClass('is-active');
-    $('.main-menu__inner').addClass('is-overflow');
-    $(this).next('.main-menu__dropdown').addClass('is-open');
-  }
-  return false;
-});
-
-$(document).on('click', '.js-menu-lvl2-close', function () {
-  $('.js-menu-lvl2').removeClass('is-active');
-  $('.main-menu__inner').removeClass('is-overflow');
-  $('.main-menu__dropdown').removeClass('is-open');
-  return false;
-});
-
-//открытие поиска
-$(document).on('click', '.js-search-opener', function () {
+$(document).on('click', '.js-sublist-toggler', function () {
   const _this = $(this);
   if(!_this.hasClass('is-active')) {
-    $('body').addClass('is-overflow');
-    $('.header__dropdown.search-popup').addClass('is-open');
+    _this.closest('.main-menu__root-item').find('.main-menu__sublist').slideDown();
+    _this.find('use').attr('xlink:href', 'images/sprite.svg#minus-icon');
     _this.addClass('is-active');
   } else {
-    _this.removeClass('is-active');
-    $('body').removeClass('is-overflow');
-    $('.header__dropdown.search-popup').removeClass('is-open');
-  }
-
-  return false;
-});
-
-//закрытие поиска
-$(document).on('click', '.js-search-closer', function () {
-  $('body').removeClass('is-overflow');
-  $('.header__dropdown.search-popup').removeClass('is-open');
-  $('.js-search-opener').removeClass('is-active');
-  return false;
-});
-
-//варианты быстрого поиска
-$(document).on('click', '.search-option', function() {
-  let query = $(this).text();
-  $('.search-form__input').val(query).focus();
-});
-
-//аккордион
-$(document).on('click', '.js-accordion-toggler', function () {
-  let _this = $(this);
-  if(!_this.hasClass('is-active')){
-    _this.closest('.accordion').find('.accordion__body').slideDown();
-    _this.addClass('is-active');
-  }else{
-    _this.closest('.accordion').find('.accordion__body').slideUp();
+    _this.closest('.main-menu__root-item').find('.main-menu__sublist').slideUp();
+    _this.find('use').attr('xlink:href', 'images/sprite.svg#plus-icon');
     _this.removeClass('is-active');
   }
+
   return false;
 });
 
