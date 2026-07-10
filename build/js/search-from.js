a6af6f1514a3eb9a5d909c327a05786317af3f85
mazax1ct <vova@giveandget.ru>
2026-07-10 17:43:58 +0300

    catalog + object-card fav

--- build/js/search-from.js
+++ build/js/search-from.js
@@ -14,9 +14,34 @@ dpMin = new AirDatepicker('#date-in', {
         dpMax.clear();
       }
     }
-})
+});
 
 dpMax = new AirDatepicker('#date-out', {
     isMobile: true,
     autoClose: true,
-})
+    minDate: [new Date()]
+});
+
+$(document).on('click', '.js-filter-opener', function() {
+  $('.page').addClass('is-overflow');
+  $('.js-filter').fadeIn();
+  return false
+});
+
+$(document).on('click', '.js-filter-closer', function() {
+  $('.page').removeClass('is-overflow');
+  $('.js-filter').fadeOut();
+  return false
+});
+
+$(document).on('click', '.js-sorting-opener', function() {
+  $('.page').addClass('is-overflow');
+  $('.js-sorting').fadeIn();
+  return false
+});
+
+$(document).on('click', '.js-sorting-closer', function() {
+  $('.page').removeClass('is-overflow');
+  $('.js-sorting').fadeOut();
+  return false
+});
